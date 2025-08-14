aafde96f2fd6c3dbc5f339435d2535b93c5b98d0
Ari Ardiansyah <ariardiansyah101@gmail.com>
2025-08-14 18:02:46 +0000

    The `npm install` command failed in my project. Analyze the following er

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,20 @@
 
 import { PrismaClient } from '@prisma/client';
 
-export { PrismaClient };
+// This is the correct way to instantiate Prisma Client in Next.js.
+// It prevents multiple instances of Prisma Client from being created in development.
+// See: https://www.prisma.io/docs/support/help-articles/nextjs-prisma-client-dev-practices
+
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    // log: ['query'],
+  });
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
